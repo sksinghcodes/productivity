e787036ad7f307503014ba10bd64824d332a5a78
Surajkant Singh <sksinghcodes@gmail.com>
2025-05-11 10:06:16 +0530

    added routing

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import { reactRouter } from '@react-router/dev/vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [reactRouter()],
+});
